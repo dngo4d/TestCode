311ad7b2908090334cd6d545f245e37ee7a4b51f
Danny Ngo <dngo4d@gmail.com>
2025-04-21 12:11:17 -0700

    Add Order Controller

---
+++ EF_DTO_API_V2/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using EF_DTO_API_V2.DTO;
+using EF_DTO_API_V2.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EF_DTO_API_V2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ResponseCache(Duration = 300, Location = ResponseCacheLocation.Any)]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<OrderDTO>>> GetAll()
+        {
+            return Ok(await _orderService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderDTO>> Get(int id)
+        {
+            try
+            {
+                return Ok(await _orderService.GetByIdAsync(id));
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+
+
+    }
+}
