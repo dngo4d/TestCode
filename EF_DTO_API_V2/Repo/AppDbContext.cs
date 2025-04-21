978e9739bf62f88a5b3b325b532c82b111df2fd8
Danny Ngo <dngo4d@gmail.com>
2025-04-21 11:05:10 -0700

    Add files via upload

---
+++ EF_DTO_API_V2/Repo/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EF_DTO_API_V2.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_DTO_API_V2.Repo
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
