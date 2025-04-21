434edab90d337e5fabbb2cd091478654480c64d2
Danny Ngo <dngo4d@gmail.com>
2025-04-21 11:04:22 -0700

    Add files via upload

---
+++ EF_DTO_API_V2/DTO/ProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace EF_DTO_API_V2.DTO
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+        public string ProductType { get; set; }
+        public decimal Price { get; set; }
+        public int CompanyId { get; set; }
+
+    }
+}
