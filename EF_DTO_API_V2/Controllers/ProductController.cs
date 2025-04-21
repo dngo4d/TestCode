ca3cd49ec54ec1683584171fdd7f7de032214890
Danny Ngo <dngo4d@gmail.com>
2025-04-21 11:05:32 -0700

    Add files via upload

---
+++ EF_DTO_API_V2/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using EF_DTO_API_V2.DTO;
+using EF_DTO_API_V2.Service;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EF_DTO_API_V2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ResponseCache(Duration = 300, Location = ResponseCacheLocation.Any)]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ProductDTO>>> GetAll()
+        {
+            return Ok(await _productService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDTO>> Get(int id)
+        {
+            try
+            {
+                return Ok(await _productService.GetByIdAsync(id));
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+
+        
+    }
+}
